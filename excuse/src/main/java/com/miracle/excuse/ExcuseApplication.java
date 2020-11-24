25bc5c46ec94491ba506af80e4d442049c20ebd8
sqq <437606847@qq.com>
2020-11-24 13:10:34 +0800

    Initial commit

---
+++ excuse/src/main/java/com/miracle/excuse/ExcuseApplication.java
@@ -0,0 +1,13 @@
+package com.miracle.excuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcuseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExcuseApplication.class, args);
+  }
+
+}
